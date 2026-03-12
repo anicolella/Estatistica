b9ad11e2edaac87d076358ddac7d87ecb4a500f9
anicolella <nicolella@usp.br>
2026-03-12 16:15:30 -0300

    Criacao do site

---
+++ pacotesR.R
@@ -0,0 +1,8 @@
+library(plot3d)
+install.packages("plot3D")
+install.packages("remotes")
+
+remotes::install_version("intoo", version = "0.4.0")
+remotes::install_version("kubik", version = "0.3.0")
+remotes::install_version("barsurf", version = "0.7.0")
+remotes::install_version("bivariate", version = "0.7.0")
